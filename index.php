05fda7520d6c81e86cbbd778a94cee343699e888
Gabriel Neto <gabrielsebastiao321@gmail.com>
2023-09-10 10:28:37 -0300

    adicionando template de login, estilos e imgs

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="img/icone.jpg" type="image/x-icon">
+    <title>Login Template</title>
+</head>
+<body>
+    <div class="login">
+        <div class="form">
+            <img class="logo_login" src="img/logo.jpg" alt="template de logo com fundo branco sem conteúdo">
+            <form>
+                <input type="email" name="usuario" placeholder="Seu email">
+                <input type="password" name="senha" placeholder="Senha">
+                <button>Login</button>
+            </form>
+        </div>
+    </div>
+    
+</body>
+</html>
